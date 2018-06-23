e94d056d9699f37740b68b6d2bd0a7d8501cfabb
Charlike Mike Reagent <olsten.larck@gmail.com>
2018-06-23 05:14:17 +0300

    chore: update numbers
    Signed-off-by: Charlike Mike Reagent <olsten.larck@gmail.com>

--- test/index.js
+++ test/index.js
@@ -2,28 +2,39 @@
 
 const nanomorph = require('nanomorph');
 const minmorph = require('../src/index');
-const abstractMorph = require('./test-morph'); // 31/31 passing
-const abstractKeyed = require('./test-keyed'); // 14/19 passing
-const abstractFuzz = require('./test-fuzz'); // 422/422 passing
-const abstractEvents = require('./test-events'); // 93/93 passing
+const abstractMorph = require('./test-morph');
+const abstractKeyed = require('./test-keyed');
+const abstractFuzz = require('./test-fuzz');
+const abstractEvents = require('./test-events');
 
 /**
  * MINmorph
  */
 
-minmorph.__name = 'minMorph'; // 499/565 passing
+// 719 of 724 passing
+// because exactly 2 `keyed` tests
+minmorph.__name = 'minMorph';
 
 /**
  * NANO MORPH
  */
 
-nanomorph.__name = 'nanomorph'; // 565/565 passing
+// 620 of 724 passing
+// because some `events` tests
+nanomorph.__name = 'nanomorph';
 
 /**
  * RUN TESTS
  */
 
+// 31 of 31 passing
 abstractMorph(minmorph);
+
+// 422 of 422 passing
 abstractFuzz(minmorph);
+
+// 249 of 249 passing
 abstractEvents(minmorph);
+
+// 17 of 22 passing
 abstractKeyed(minmorph);
